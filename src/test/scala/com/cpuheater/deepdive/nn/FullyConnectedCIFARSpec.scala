7517d4ac49f53c7a5dd8861d0b40b712b1d32ca3
luke <cpuheater@gmail.com>
2017-12-31 10:38:17 +0100

    working fullyconnected

--- src/test/scala/com/cpuheater/deepdive/nn/FullyConnectedCIFARSpec.scala
+++ src/test/scala/com/cpuheater/deepdive/nn/FullyConnectedCIFARSpec.scala
@@ -15,22 +15,23 @@ import org.nd4s.Implicits._
 
 class FullyConnectedCIFARSpec extends TestSupport{
 
-  it should "cross entropy 0.9838" in {
+
+  it should "cifar50-loss: 1.1513407342135907E-4" in {
 
     val height: Int = 32
     val width: Int = 32
     val channels: Int = 3
     val numLabels: Int = CifarLoader.NUM_LABELS
-    val numSamples: Int = 4
-    val batchSize: Int = 4
+    val numSamples: Int = 50
+    val batchSize: Int = 50
 
 
 
-    val cifar = new CifarDataSetIterator(batchSize, numSamples, Array[Int](height, width, channels), true, true)
+    //val cifar = new CifarDataSetIterator(batchSize, numSamples, Array[Int](height, width, channels), true, true)
     //val cifarEval = new CifarDataSetIterator(batchSize, 100, Array[Int](height, width, channels), true, false)
 
-    val ala = cifar.next(1)
-    val (features, labels) = readFromFile
+   // val ala = cifar.next(1)
+    val (features, labels) = readFromFile("/cifar50.csv")
     val nbOfExamples = features.rows()
 
 
@@ -67,10 +68,61 @@ class FullyConnectedCIFARSpec extends TestSupport{
 
   }
 
-  def readFromFile: (INDArray, INDArray) = {
+  it should "cross entropy 0.9838" in {
+
+    val height: Int = 32
+    val width: Int = 32
+    val channels: Int = 3
+    val numLabels: Int = CifarLoader.NUM_LABELS
+    val numSamples: Int = 4
+    val batchSize: Int = 4
+
+
+
+    val cifar = new CifarDataSetIterator(batchSize, numSamples, Array[Int](height, width, channels), true, true)
+    //val cifarEval = new CifarDataSetIterator(batchSize, 100, Array[Int](height, width, channels), true, false)
+
+    val ala = cifar.next(1)
+    val (features, labels) = readFromFile("/cifar4.csv")
+    val nbOfExamples = features.rows()
+
+
+    val loss = SoftMaxLoss
+    val lr = 1e-3
+
+    val inputSize=3*32*32
+
+
+    val hiddenSize = 100
+
+    val model = Sequential()
+      .add(Linear(inputSize, hiddenSize , activation = ReLU))
+      .add(Linear(hiddenSize, hiddenSize, activation = ReLU))
+      .add(Linear(hiddenSize, numLabels))
+      .build(loss, lr, batchSize, seed=Some(1), numOfEpoch = 10)
+
+
+    model.fit(new DataSet(features, labels))
+
+    //println(model.params())
+
+    /*val dataSet = cifar.next(numSamples)
+    val features = dataSet.getFeatureMatrix
+    val labels = dataSet.getLabels
+*/
+    val pred = model.predict(features)
+    val indicies = Nd4j.argMax(pred, 1)
+    val lindicies = Nd4j.argMax(labels, 1)
+
+    println(indicies)
+    println(lindicies)
+
+  }
+
+  def readFromFile(name: String): (INDArray, INDArray) = {
 
     val oneHotMap = Nd4j.eye(10)
-    val lines = io.Source.fromInputStream(getClass.getResourceAsStream("/cifar4.csv")).getLines()
+    val lines = io.Source.fromInputStream(getClass.getResourceAsStream(name)).getLines()
 
     val (features, labelsArray) = lines.map { x =>
       val value = x.split(",")
